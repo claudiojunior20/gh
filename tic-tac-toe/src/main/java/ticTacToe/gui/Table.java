a8ec9a3c94ece2c391e2d27e61992366c869cc1b
Claudio <jukusma20@gmail.com>
2025-05-27 22:52:18 -0300

    tic aula 1

---
+++ tic-tac-toe/src/main/java/ticTacToe/gui/Table.java
@@ -0,0 +1,19 @@
+package ticTacToe.gui;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.ImageIcon;
+
+public class Table implements Paintable{
+	private Point position;
+	private Dimension dimension;
+	private ImageIcon icon;
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
