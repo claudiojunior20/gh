c3f839accae976fd1385452f36171c67695c99e1
Claudio <jukusma20@gmail.co>
2025-06-02 18:34:34 -0300

    teste

--- tic-tac-toe/src/main/java/ticTacToe/component/button.java
+++ tic-tac-toe/src/main/java/ticTacToe/component/button.java
@@ -1,9 +1,12 @@
 package ticTacToe.component;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Point;
 
-public class button {
+import ticTacToe.gui.Paintable;
+
+public class button implements Paintable{
 	
 	 private Point position= null;
 	 private Dimension  dimension= null;
@@ -26,5 +29,37 @@ public class button {
 		 
 	 }
 	 
-	 public void 
+	 public void setPosition(int x, int y) {
+		 this.position = new Point (x , y);
+	 }
+	 
+	 
+	 public Point getPosition() {
+		 return (Point) this.position.clone();
+	 }
+	 
+	 
+	 public void setSize(int width, int height) {
+		 this.dimension = new Dimension(width, height);
+	 }
+	 
+	 public Dimension getsize() {
+		 return (Dimension) this.dimension.clone();
+	 }
+	 
+	 public int width() {
+		 return this.dimension.width;
+	 }
+	 
+	 public int height() {
+		 return this.dimension.height;
+	 }
+
+
+	@Override
+	public void paint(Graphics g) {
+		g.drawRect(position.x, position.y, dimension.width, dimension.height);
+		
+	}
+	 
 }
---
+++ tic-tac-toe/src/test/java/appTest/AppButtonPaintableTest.java
@@ -0,0 +1,19 @@
+package appTest;
+
+import ticTacToe.gui
+
+public class AppButtonPaintableTest {
+   public static void main(String[]args) {
+	   
+	   MyWindow window = new MyWindow();
+	   Button button = new Button();
+	   window.add(button);
+	   
+	   
+	   button = new Button(100,150);
+	   window.add(button);
+	   
+	   button = new Button(200,200,50,50);
+	   window.add(button);
+   }
+}
